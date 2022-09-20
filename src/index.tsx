10d683689b4a3ef52c69c0d2c019733c71c9c66b
[Ivan Horvat] <development@redsync.studio>
2022-09-20 19:13:34 +0200

    Removing unnecessary dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-const queryClient = new QueryClient();
-
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
